88d7f5868d02b107cac8e0d65a20ef0f87c9a992
rchoud19 <rchoud19@ms.ds.uhc.com>
2018-05-17 11:06:48 +0530

    ApacheIgniteBook

---
+++ chapters/README.md
@@ -0,0 +1,10 @@
+## Prerequisites
+| N | Name    | Value                                                                                |
+|---|---------|--------------------------------------------------------------------------------------|
+| 1 | JDK     | Oracle JDK 8 and above                                                               |
+| 2 | OS      | Linux, MacOS (10.6 and above), Windows XP and above, Windows Server (2008 and above) |
+|3  | Ignite version| 1.6 or above |
+
+> To compile, run the following command:
+
+> mvn clean install
---
+++ chapters/chapter-dist-computing/src/main/java/com/blu/imdg/example8/TestAccountSavingsMain.java
@@ -0,0 +1,52 @@
+package com.blu.imdg.example8;
+
+import com.blu.imdg.common.CommonConstants;
+import com.blu.imdg.common.bankData.BankDataGenerator;
+import com.blu.imdg.common.bankData.model.*;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCompute;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by mikl on 21.10.16.
+ */
+
+//mvn exec:java -Dexec.mainClass=com.blu.imdg.example8.TestAccountSavingsMain
+public class TestAccountSavingsMain {
+
+    public static void main(String[] args) {
+
+        try (Ignite ignite = Ignition.start(CommonConstants.CLIENT_CONFIG)) {
+            IgniteCompute compute = ignite.compute();
+
+            IgniteCache<AccountCacheKey, AccountCacheData> cache = BankDataGenerator.createBankCache(ignite);
+            IgniteCache<String, CashBackDictionaryData> savingsCache = BankDataGenerator.initSavigsCache(ignite);
+
+            SqlFieldsQuery sql = new SqlFieldsQuery("select * from TransactionData where account = ?");
+
+            BigDecimal result = compute.affinityCall(BankDataGenerator.ACCOUNT_CACHE, new AccountKey(BankDataGenerator.TEST_ACCOUNT), () -> {
+                //download all transactions for this account
+                List<List<?>> data = cache.query(sql.setArgs(BankDataGenerator.TEST_ACCOUNT)).getAll();
+                BigDecimal cashBack = new BigDecimal(0);
+                for (List row : data) {
+                    TransactionKey key = (TransactionKey) row.get(0);
+                    TransactionData tr = (TransactionData) row.get(1);
+
+                    CashBackDictionaryData cashBackDictionaryData = savingsCache.get(tr.getTransactionType());
+                    cashBack = cashBack.add(tr.getSum().multiply(cashBackDictionaryData.getCashBackPercent()));
+                }
+                //System.out.println("savings="+cashBack);
+                return cashBack;
+            });
+
+            System.out.println("CashBack="+result);
+
+        }
+    }
+
+}
